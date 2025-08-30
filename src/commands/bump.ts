bf4400c209fa63d7931b769361cb6145cca06577
Murimi-Njiru <murimi@hotmail.com>
2025-08-30 09:13:43 +0300

    updated

--- src/commands/bump.ts
+++ src/commands/bump.ts
@@ -81,7 +81,10 @@ const bump = async (type?: string, force?: boolean): Promise<void> => {
           type = "PRE_RELEASE";
         }
         else {
-          if(!force) {
+          if(config.releaseBranch === branch) {
+            type = "PATCH";
+          }
+          else if(!force) {
             type = await select({message: 'Select a bump type', choices: [{name: 'major', value: 'MAJOR'}, {name: 'minor', value: 'MINOR'}, {name: 'patch', value: 'PATCH'}]})
           }
           else {
